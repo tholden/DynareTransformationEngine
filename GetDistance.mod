tholden/DynareTransformationEngine

commit a74101d24fd16d12079b114d562782ef7ac59685

correct GetDistance when dimension>1

GetDistance.mod
@@ -2,7 +2,7 @@
 @#define Indices2 = IndicesArray[ ((Point2-1)*SpatialDimensions+1):(Point2*SpatialDimensions) ]
 @#define Distance = "( 0 "
 @#for Dimension in 1 : SpatialDimensions
-    @#define DistanceTemp = "abs( " + Numbers[Indices1] + "/" + Numbers[SpatialPointsPerDimension+1] + " - " + Numbers[Indices2] + "/" + Numbers[SpatialPointsPerDimension+1] + " )"
+    @#define DistanceTemp = "abs( " + Numbers[Indices1[Dimension]] + "/" + Numbers[SpatialPointsPerDimension+1] + " - " + Numbers[Indices2[Dimension]] + "/" + Numbers[SpatialPointsPerDimension+1] + " )"
     @#if SpatialShape[1] == "P"
         @#define Distance = Distance + " + " + DistanceTemp + " ^ ( " + SpatialNorm + " )"
     @#else

commit a72c06005980b455b8939bae0011ad1ce86dc9bf

fix bug introduced in a74101d24fd16d12079b114d562782ef7ac59685 with dim 1

GetDistance.mod
@@ -1,5 +1,5 @@
-@#define Indices1 = IndicesArray[ ((Point1-1)*SpatialDimensions+1):(Point1*SpatialDimensions) ]
-@#define Indices2 = IndicesArray[ ((Point2-1)*SpatialDimensions+1):(Point2*SpatialDimensions) ]
+@#define Indices1 = [ IndicesArray[ ((Point1-1)*SpatialDimensions+1):(Point1*SpatialDimensions) ] ]
+@#define Indices2 = [ IndicesArray[ ((Point2-1)*SpatialDimensions+1):(Point2*SpatialDimensions) ] ]
 @#define Distance = "( 0 "
 @#for Dimension in 1 : SpatialDimensions
     @#define DistanceTemp = "abs( " + Numbers[Indices1[Dimension]] + "/" + Numbers[SpatialPointsPerDimension+1] + " - " + Numbers[Indices2[Dimension]] + "/" + Numbers[SpatialPointsPerDimension+1] + " )"